82bf0d2f27d15ce2b3a86380a521e952b0b1ad7c
Ivan Ahmed Adi <ivo.adi_18@hotmail.com.ar>
2022-05-15 22:06:11 -0300

    entrega2

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>WaterStore</h1>
+            <div>
+                <button>Phones</button>
+                <button>Tablets</button>
+                <button>Smarts</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
