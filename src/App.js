20b3b9ddc5efb770f228972bae526b9fb66b58b2
Ivan Ahmed Adi <ivo.adi_18@hotmail.com.ar>
2022-05-17 22:22:10 -0300

    carrito

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/Navbar/NavBar.js';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <NavBar />
+      <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,10 +1,12 @@
 import './NavBar.css'
+import { FaDollyFlatbed } from "react-icons/fa";
 
 const NavBar = () => {
     return (
         <nav className="navv">
             <h1 className="huno">WaterStore</h1>
             <div className="divnav">
+                <button className="btn"><FaDollyFlatbed />5</button>
                 <button className="btn">Phones</button>
                 <button className="btn">Tablets</button>
                 <button className="btn">Smarts</button>
