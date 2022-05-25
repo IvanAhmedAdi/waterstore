5e218158672de6404907240059204e638a663f90
Ivan Ahmed Adi <ivo.adi_18@hotmail.com.ar>
2022-05-25 20:02:56 -0300

    cambio

--- src/components/ItemListConteiner/ItemListConteiner.js
+++ src/components/ItemListConteiner/ItemListConteiner.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React from 'react'
+import './ItemListConteiner.css'
+import { useState, useEffect } from 'react'
+import { getProducts } from '../../asyncmock'
+import ItemList from '../ItemList/ItemList'
 
+const ItemListConteiner = ({ greeting }) => {
+    const [products, setProducts] = useState([])
 
-const ItemListConteiner = (props) =>{
-    return <h1>
-        {props.greeting}
-    </h1>
+    useEffect(() => {
+        getProducts().then(response => {
+            setProducts(response)
+        })
+    }, [])
+   
+    console.log('antes de montar')
+
+    return(
+        <div className='ItemListConteiner'>
+            <h1>{ greeting }</h1>
+            {/* { products.map(product => <p key={product.id}>{product.name}</p>) } */}
+            <ItemList products={products}/>
+        </div>
+    )
 }
-export default ItemListConteiner;
+
+export default ItemListConteiner
