9091e9805e1b47f0937a534078e6de36e4c8b335
Ivan Ahmed Adi <ivo.adi_18@hotmail.com.ar>
2022-06-01 17:27:23 -0300

    productos

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,12 +1,29 @@
 const products = [
     { 
         id: '1', 
-        name: 'iphone13', 
+        name: 'Iphone13', 
         price: 1000, 
         category: 'celular', 
         img: '/images/iphone13.png', 
         stock: 10, 
         description:'Descripcion de Iphone 13'
+    }, 
+    { 
+        id: '2', 
+        name: 'Tablet', 
+        price: 1000, 
+        category: 'tablet', 
+        img: '/images/tablet.png', 
+        stock: 15, 
+        description:'Descripcion de Tablet'
+    }, { 
+        id: '3', 
+        name: 'SmartTv', 
+        price: 1000, 
+        category: 'tablet', 
+        img: '/images/tv.png', 
+        stock: 10, 
+        description:'Descripcion de SmartTv'
     },
 ];
 
